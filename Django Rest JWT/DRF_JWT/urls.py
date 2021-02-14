1b46199f298f39677d1e6baf6cef583af54e5be7
abhisheksahu92 <70888785+abhisheksahu92@users.noreply.github.com>
2021-02-14 20:20:44 +0530

    Created Django Rest JWT.

---
+++ Django Rest JWT/DRF_JWT/urls.py
@@ -0,0 +1,32 @@
+"""DRF_JWT URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
+
+from Person.views import PersonViews
+router = routers.DefaultRouter()
+
+router.register('person',PersonViews,basename='person')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('gettoken/',TokenObtainPairView.as_view(),name='gettoken'),
+    path('refreshtoken/',TokenRefreshView.as_view(),name='refreshtoken'),
+    path('verifytoken/',TokenVerifyView.as_view(),name='verifytoken'),
+    path('',include(router.urls))
+]
